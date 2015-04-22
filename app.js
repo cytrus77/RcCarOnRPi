9ca87212f700b332251c7ec28c985b188a1007a9
cytrus77 <cytrus77@gmail.com>
2015-04-22 13:51:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ function handler(request, response)
 
 var pin_przyspieszenie = 17;
 var pin_skretu = 18;
-var pin_przod = 19;
-var pin_wsteczny = 26;
-var pin_wlewo = 16;
-var pin_wprawo = 20;
+var pin_przod = 4;//19;
+var pin_wsteczny = 4;//26;
+var pin_wlewo = 4;//16;
+var pin_wprawo = 4;//20;
 var logcount = 0;
 var przyspieszenie = 0; //w %
 var skret = 0;
